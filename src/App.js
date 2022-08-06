95e8489400ffff4759b7c53b6b74af43dd27e7ba
Belaid Hafedh <belaidhafedh2@gmail.com>
2022-08-06 09:35:22 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+<div className="App">
+<div class="form-group">
+  <label for="usr">Name:</label>
+  <input type="text" class="form-control" id="usr"></input>
+</div>
+<div class="form-group">
+  <label for="usr">username:</label>
+  <input type="text" class="form-control" id="usr"></input>
+</div>
+<button type="button" class="btn btn-primary">Primary</button>
+</div>
+
+  );
+}
+
+export default App;
